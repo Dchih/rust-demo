0860e1bdb23ab11ef883a5560e4c47a5110c79bb
Dchih <duzchih@gmail.com>
2024-09-17 20:34:46 +0800

    init

---
+++ target/debug/deps/rust-58c83e2b167c44c4.d
@@ -0,0 +1,5 @@
+C:\Users\Dchih\Desktop\rust-demo\target\debug\deps\librust-58c83e2b167c44c4.rmeta: src/main.rs
+
+C:\Users\Dchih\Desktop\rust-demo\target\debug\deps\rust-58c83e2b167c44c4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust-b9d7508424704fdb.d
@@ -0,0 +1,5 @@
+C:\Users\Dchih\Desktop\rust-demo\target\debug\deps\librust-b9d7508424704fdb.rmeta: src/main.rs
+
+C:\Users\Dchih\Desktop\rust-demo\target\debug\deps\rust-b9d7508424704fdb.d: src/main.rs
+
+src/main.rs:
